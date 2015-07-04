c61e388938ad0d98e70755d76e2c5d4a9b418d50
ksrt12 <kazakov12stepan2012@live.ru>
2015-07-04 13:25:49 +0500

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,9 +1,1 @@
-# This whole script is a hack. Hoping to replace it soon.
-if [ ! -d device/huawei/u8950/ok ]
-then
-sh device/huawei/msm7x27a-common/patches/apply.sh
-mkdir device/huawei/u8950/ok
-else
-echo "msm7x27a patches already applied"
-fi
-
+add_lunch_combo cm_u8950-userdebug
