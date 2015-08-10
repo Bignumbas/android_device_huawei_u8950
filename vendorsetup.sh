a2e55becaff1164412c5943159e5b2513bee45fa
Kazakov Stepan <kazakov12stepan2012@live.ru>
2015-08-10 13:54:40 +0500

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -3,9 +3,5 @@ then
 mkdir device/huawei/u8950/ok
 sh device/huawei/msm7x27a-common/patches/apply.sh
 fi
-if [ ! -e ./kernel/huawei/huawei-kernel-3.4/arch/arm/configs/u8950_defconfig ]
-then
-cp -f ./device/huawei/u8950/configs/u8950_defconfig ./kernel/huawei/huawei-kernel-3.4/arch/arm/configs/u8950_defconfig
-fi
 add_lunch_combo slim_u8950-userdebug
 #add_lunch_combo slim_u8950-eng
